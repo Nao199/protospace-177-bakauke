737510b6d3d0cbca7af7ec7ee57a0d29d32e42e2
SHUcocoai <chongjieyiteng@gmail.com>
2024-08-12 11:50:39 +0900

    投稿機能のコントローラーの設定と投稿フォームの部分テンプレートファイル指定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,18 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+  end
+
+  Private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
+  end
 end
