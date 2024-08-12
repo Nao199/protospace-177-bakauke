97d26d4f12e6535bba41839aa3bea9b9925017a7
SHUcocoai <chongjieyiteng@gmail.com>
2024-08-12 16:03:14 +0900

    Activestrageの導入と未ログイン時のリダイレクト、投稿失敗時の

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,12 +7,16 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    Prototype.create(prototype_params)
+    if Prototype.create(prototype_params)
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
