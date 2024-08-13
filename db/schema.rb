e83446dc41d7d326baad5ea3260fd49afcc938c2
Nao199 <eddionk13@gmail.com>
2024-08-13 09:51:46 +0900

    マイグレーション再実行

--- db/schema.rb
+++ db/schema.rb
@@ -39,17 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_12_040913) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "protos", charset: "utf8mb4", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "prototypes", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "catch_copy", null: false
