782c209e954099a941723e234abb8b26534893eb
SHUcocoai <chongjieyiteng@gmail.com>
2024-08-13 17:05:42 +0900

    モデル作成とアソシエーション設定とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
   resources :prototypes, only: [:index, :new, :create, :show]
+    resources :comments, only: :create
 end
 
