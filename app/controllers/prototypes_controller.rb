4440e56826ff45dddba35b30e89a98fa20f11e95
SHUcocoai <chongjieyiteng@gmail.com>
2024-08-15 12:26:05 +0900

    robocopによる軽微な修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -45,9 +45,9 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    return if user_signed_in?
+
+    redirect_to action: :index
   end
 
   private
